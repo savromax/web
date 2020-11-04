328066209999b957e131c19dc229e7d5b15ebdcc
savromax <68664083+savromax@users.noreply.github.com>
2020-11-04 13:45:26 +0300

    1

--- brutal/js/scripts.js
+++ brutal/js/scripts.js
@@ -13,10 +13,16 @@ $( document ).ready(function() {
 	});
 	
 	$('.faq-item-title').click(function() {
-		$('.faq-item-title').removeClass('active');
-		$('.faq-item-content').slideUp();
-		$(this).addClass('active');
-		$(this).parent().find('.faq-item-content').slideDown();
+		if ($(this).hasClass('active')) {
+			$(this).removeClass('active');
+			$('.faq-item-content').slideUp();			
+			$(this).parent().find('.faq-item-content').slideUp();
+		} else {
+			$('.faq-item-title').removeClass('active');
+			$('.faq-item-content').slideUp();
+			$(this).addClass('active');
+			$(this).parent().find('.faq-item-content').slideDown();
+		}
 	});
 	
 	$('.mob-nav').click(function() {
@@ -25,48 +31,63 @@ $( document ).ready(function() {
 	
 });
 
-wow = new WOW(
-  {
-	animateClass: 'animate__animated',
-	offset:       0,
-	mobile: 0
-  }
-);
-wow.init();
 
-// ANIMATE
-var icon1 = document.querySelector('.about-icon1');
-var icon2 = document.querySelector('.about-icon2');
-var icon3 = document.querySelector('.about-icon3');
-var icon4 = document.querySelector('.about-icon4');
-var icon5 = document.querySelector('.about-icon5');
-var icon6 = document.querySelector('.about-icon6');
-var icon7 = document.querySelector('.about-icon7');
-var icon8 = document.querySelector('.about-icon8');
-var icon9 = document.querySelector('.about-icon9');
-var sicon1 = document.querySelector('.sub-icon1');
-var sicon2 = document.querySelector('.sub-icon2');
-var sicon3 = document.querySelector('.sub-icon3');
-var sicon4 = document.querySelector('.sub-icon4');
-var sicon5 = document.querySelector('.sub-icon5');
-var sicon6 = document.querySelector('.sub-icon6');
-window.addEventListener('mousemove', function(e) {
-	var x = e.clientX / window.innerWidth;
-	var y = e.clientY / window.innerHeight;  
-	icon1.style.transform = 'translate(-' + x * 50 + 'px, -' + y * 50 + 'px)';
-	icon2.style.transform = 'translate(+' + x * 50 + 'px, -' + y * 50 + 'px)';
-	icon3.style.transform = 'translate(+' + x * 50 + 'px, -' + y * 50 + 'px)';
-	icon4.style.transform = 'translate(-' + x * 50 + 'px, +' + y * 50 + 'px)';
-	icon5.style.transform = 'translate(+' + x * 50 + 'px, +' + y * 50 + 'px)';
-	icon6.style.transform = 'translate(-' + x * 50 + 'px, -' + y * 50 + 'px)';
-	icon7.style.transform = 'translate(+' + x * 50 + 'px, +' + y * 50 + 'px)';
-	icon8.style.transform = 'translate(-' + x * 50 + 'px, -' + y * 50 + 'px)';
-	icon9.style.transform = 'translate(+' + x * 50 + 'px, +' + y * 50 + 'px)';
-	sicon1.style.transform = 'translate(-' + x * 15 + 'px, -' + y * 15 + 'px)';
-	sicon2.style.transform = 'translate(+' + x * 15 + 'px, -' + y * 15 + 'px)';
-	sicon3.style.transform = 'translate(+' + x * 15 + 'px, +' + y * 15 + 'px)';
-	sicon4.style.transform = 'translate(-' + x * 15 + 'px, +' + y * 15 + 'px)';
-	sicon5.style.transform = 'translate(-' + x * 25 + 'px, -' + y * 25 + 'px)';
-	sicon6.style.transform = 'translate(-' + x * 15 + 'px, +' + y * 15 + 'px)';
+/*YaMapsShown = false;
+
+$(document).ready(function () {
+  var target = $('.block3-block');
+  var targetPos = target.offset().top;
+  var winHeight = $(window).height();
+  var scrollToElem = targetPos - winHeight;
+ $(window).scroll(function() {
+    var winScrollTop = $(this).scrollTop();
+    if (!YaMapsShown){
+     if (winScrollTop > scrollToElem) {      
+      $('.block3-image-image-fix').addClass('slideInDown');
+     }
+    }
+ });
+});*/
+
+
+
+$(window).scroll(function(){
+	var el = $('.block3-item-title');
+    if ($(this).scrollTop() > el.offset().top - 200 ) {
+        el.addClass('bounceIn');
+    }
 });
 
+
+var element_point_about = $('.about').offset().top; // положение элемента относительно начала страницы
+var element_animated_about = false; // чтобы анимация происходила только один раз, используем эту переменную-флаг
+var animate_delay_about = 100; // задержка анимации (через 100 пикселей)
+ 
+$(window).scroll(function() {
+  // проверяем, прокрутили ли страницу до элемента
+  if (!element_animated_about && $(window).scrollTop() + window.innerHeight > element_point_about + animate_delay_about) {
+    element_animated_about = true; // отмечаем, что анимация выполнена
+ 
+    $(".scroll_about1").paroller({ factor: 0.1, factorXs: 0.2, type: 'foreground', direction: 'vertical' });
+	$(".scroll_about2").paroller({ factor: 0.2, factorXs: 0.2, type: 'foreground', direction: 'vertical' });
+	$(".scroll_about3").paroller({ factor: 0.3, factorXs: 0.2, type: 'foreground', direction: 'vertical' });
+     
+  }
+});
+
+
+var element_point_sub = $('.sub').offset().top; // положение элемента относительно начала страницы
+var element_animated_sub = false; // чтобы анимация происходила только один раз, используем эту переменную-флаг
+var animate_delay_sub = 100; // задержка анимации (через 100 пикселей)
+
+$(window).scroll(function() {
+  // проверяем, прокрутили ли страницу до элемента
+  if (!element_animated_sub && $(window).scrollTop() + window.innerHeight > element_point_sub + animate_delay_sub) {
+    element_animated_sub = true; // отмечаем, что анимация выполнена
+ 
+    $(".scroll_sub1").paroller({ factor: 0.1, factorXs: 0.2, type: 'foreground', direction: 'vertical' });
+	$(".scroll_sub2").paroller({ factor: 0.2, factorXs: 0.2, type: 'foreground', direction: 'vertical' });
+	$(".scroll_sub3").paroller({ factor: 0.3, factorXs: 0.2, type: 'foreground', direction: 'vertical' });
+     
+  }
+});
